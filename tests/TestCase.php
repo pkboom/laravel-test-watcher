54c0dddecb33559b2e64d6d97ccf4989bdf99c42
Keunbae Park <ppotpo@gmail.com>
2021-12-21 22:00:38 -0500

    wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,6 @@
 
 namespace Tests;
 
-use Illuminate\Support\Facades\File;
 use Orchestra\Testbench\TestCase as Orchestra;
 use Pkboom\TestWatcher\TestWatcherServiceProvider;
 
@@ -13,32 +12,5 @@ abstract class TestCase extends Orchestra
         return [
             TestWatcherServiceProvider::class,
         ];
-    }
-
-    protected function getEnvironmentSetUp($app)
-    {
-        $this->initializeDirectory($this->getTempDirectory());
-
-        config()->set('tinker-on-vscode.input', $this->getTempDirectory('input.php'));
-        config()->set('tinker-on-vscode.output', $this->getTempDirectory('output.json'));
-
-        config()->set('database.default', 'sqlite');
-        config()->set('database.connections.sqlite', [
-            'driver' => 'sqlite',
-            'database' => ':memory:',
-            'prefix' => '',
-        ]);
-    }
-
-    public function initializeDirectory(string $directory): void
-    {
-        File::deleteDirectory($directory);
-
-        File::makeDirectory($directory);
-    }
-
-    public function getTempDirectory(?string $file = null): string
-    {
-        return __DIR__.'/temp'.($file ? '/'.$file : '');
     }
 }
