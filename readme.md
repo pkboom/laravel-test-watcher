5210af3aed359eb04433d763692fec2126725342
Keunbae Park <ppotpo@gmail.com>
2021-12-21 21:58:20 -0500

    wip

--- readme.md
+++ readme.md
@@ -1,5 +1,13 @@
 # Laravel Test Watcher
 
+Laravel Test Watcher will rerun every time it detects changes.
+
+# Installation
+
+```bash
+composer require pkboom/laravel-test-watcher
+```
+
 ## License
 
 The MIT License (MIT). Please see [MIT license](http://opensource.org/licenses/MIT) for more information.
--- tests/TestWatcherCommandTest.php
+++ tests/TestWatcherCommandTest.php
@@ -3,8 +3,15 @@
 namespace Pkboom\TestCreator\Test;
 
 use PHPUnit\Framework\TestCase;
+use Pkboom\TestWatcher\TestWatcherCommand;
 
 class TestWatcherCommandTest extends TestCase
 {
+    /** @test */
+    public function it_can_be_instantiated()
+    {
+        $command = new TestWatcherCommand();
 
+        $this->assertInstanceOf(TestWatcherCommand::class, $command);
+    }
 }
